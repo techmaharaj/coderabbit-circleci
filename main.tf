74004e4311df6eda3099710719a2871958d5583a
Atulpriya Sharma <sharma.atulpriya@gmail.com>
2024-10-15 15:34:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,41 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "szjsdfksfdon1230asd"
-  secret_key = "328sjv9lflsdfj0sdjs"
+  region     = "us-west-2"
+  access_key = "AKIAEXAMPLE"
+  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 }
 
-resource "aws_instance" "web" {
+resource "aws_instance" "insecure_instance" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
-  security_group_ids = ["sg-0123456789abcdef0"]
+  security_group_ids = ["sg-12345678"]
+  key_name           = "unsecured-key"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Sensitive data: password123" > /etc/secrets.txt
+  EOF
 
   tags = {
-    Name = "example-instance"
+    Name = "insecure-example-instance"
+  }
+}
+
+resource "aws_security_group" "open_sg" {
+  name_prefix = "open-sg-"
+  description = "Security group with access"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
